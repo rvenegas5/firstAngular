90cdabd5345fd1755a50c8d4e5ddde25219723e0
William Venegas <rvenegas@espol.edu.ec>
2021-10-27 00:20:48 -0500

    Creating the first service and testing the products FAKEAPIs

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../services/products.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -9,14 +7,9 @@ import { tap } from 'rxjs/operators';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private productSvc: ProductsService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.productSvc.getProducts()
-    .pipe(
-      tap( res => console.log(res) )
-    )
-    .subscribe();
   }
 
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private productSVC: ProductsService) {
+
+   }
 
   ngOnInit(): void {
+    this.productSVC.getProducts()
+    .pipe( 
+      tap(res => console.log(res))
+    )
+    .subscribe();
   }
 
 }
