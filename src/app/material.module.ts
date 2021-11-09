373bb6e3bdf1e29d9fde312fb642f117d312cc1b
William Venegas <rvenegas@espol.edu.ec>
2021-11-09 00:32:51 -0500

    Styles added for checkout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,8 @@ import { MatButtonModule} from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
 @NgModule({
   exports: [
     MatToolbarModule,
@@ -12,7 +14,9 @@ import { MatRadioModule } from "@angular/material/radio";
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule
   ]
 })
 
--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -13,6 +13,46 @@ export class CheckoutComponent implements OnInit {
     city: ''
 
   }
+
+  stores = [
+    {
+      "id": 1,
+      "name": "Park Row at Beekman St",
+      "address": "38 Park Row",
+      "city": "New York",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    },
+    {
+      "id": 2,
+      "name": "Store Alcalá",
+      "address": "Calle de Alcalá, 21",
+      "city": "Madrid",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    },
+    {
+      "id": 3,
+      "name": "Chambers and West Broadway",
+      "address": "125 Chambers Street",
+      "city": "New York",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    },
+    {
+      "id": 4,
+      "name": "Covent Garden - Russell St",
+      "address": "10 Russell Street",
+      "city": "London",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    },
+    {
+      "id": 5,
+      "name": "Monmouth St",
+      "address": "11 Monmouth Street",
+      "city": "London",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    }
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
